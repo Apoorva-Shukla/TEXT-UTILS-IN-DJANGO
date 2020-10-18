b46283fc1afc31fd1c6a812155c65ab9883eabb7
Apoorva Kumar Shukla <manjusukla82@gmail.com>
2020-10-18 13:47:36 +0530

    commit 1

---
+++ text_utils/views.py
@@ -0,0 +1,54 @@
+# File By me - Apoorva
+
+from django.http import HttpResponse
+from django.shortcuts import render
+
+
+def index(request):
+    return render(request, 'index.html')
+
+def analyze(request):
+    djtext = request.POST.get('text', 'default')
+    removePunc = request.POST.get('removePunc', 'off')
+    fullcaps=request.POST.get('fullcaps','off')
+    newlineremover=request.POST.get('newlineremover','off')
+    extraspaceremover=request.POST.get('extraspaceremover','off')
+    punctuations = '''!()-[]{};:'"\,<>./?@#$%^&*_~'''
+    analyzed = ''
+
+    if removePunc == 'on':
+        for char in djtext:
+            if char not in punctuations:
+                analyzed += char
+        params = {'purpose': 'Removed Punctuations', 'analyzed_text': analyzed}
+
+        djtext = analyzed
+
+    if fullcaps=="on":
+        analyzed=""
+        for char in djtext:
+            analyzed=analyzed+char.upper()
+        params = {'purpose': 'Change To Uppercase', 'analyzed_text': analyzed}
+
+        djtext = analyzed
+
+    if newlineremover=="on":
+        analyzed=""
+        for char in djtext:
+            if char != "\n" and char != '\r':
+                analyzed=analyzed+char
+        params = {'purpose': 'Removed NewLines', 'analyzed_text': analyzed}
+
+        djtext = analyzed
+
+    if(extraspaceremover=="on"):
+            analyzed = ""
+            for index, char in enumerate(djtext):
+                if not(djtext[index] == " " and djtext[index+1]==" "):
+                    analyzed = analyzed + char
+
+            params = {'purpose': 'Removed NewLines', 'analyzed_text': analyzed}
+
+    params = {'purpose': 'Remove Punctuations', 'analyzed_text': analyzed}
+
+    return render(request, 'analyze.html', params)
